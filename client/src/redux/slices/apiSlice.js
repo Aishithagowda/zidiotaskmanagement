268e4953fb0198d18436614593c962372f2d3ef0
Aishitha V <aishitha.venkatesh4@gmail.com>
2025-05-11 17:12:59 +0530

    vite changes

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // const API_URL = import.meta.env.VITE_APP_BASE_URL + "/api";
-const API_URL = "http://localhost:8800/api";
+const API_URL = import.meta.anv.VITE_API_URL;
 
 const baseQuery = fetchBaseQuery({ baseUrl: API_URL });
 
